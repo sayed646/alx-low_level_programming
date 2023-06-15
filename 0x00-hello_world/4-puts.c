153d03b816522835df13600fa77916b266bb66f8
sayed <moka@facebook.com>
2023-06-15 09:15:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
- *describtion:a c program that print with put function
- * 
- *return: always 0 (success)
-*/
-
-       int main(void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-       int puts("\"Programming is like building a multilingual puzzle");
-       return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
