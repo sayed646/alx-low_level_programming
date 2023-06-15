b44b11cc71eb505f1e45b98e901744c9f9963533
sayed <moka@facebook.com>
2023-06-15 11:15:04 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@
     char h;
     float d;
 
-        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
+        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(h));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(h));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
 	return (0);
 }   
