098cdbb98ddf6fe41c41c0af8c46a2581b9fe1a1
sayed <moka@facebook.com>
2023-06-15 18:05:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful
+	write(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
