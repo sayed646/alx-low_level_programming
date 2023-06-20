337fb7af56b43f57a627186f1872818fe76be8fc
sayed <moka@facebook.com>
2023-06-20 01:18:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unstid.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void){
-	for(int a = 0; int b < 10; a += 1);
-	for(int a = 0; int b < 10; b += 1);
+	for(char 'a' = 0; char 'b' < 10; a += 1);
+	for(char 'a' = 0; char 'b' < 10; b += 1);
 	Console.WriteLine("{0}{1}", firstDigit, secondDigit);
 }
