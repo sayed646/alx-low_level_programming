3c21cb30aa77ff4f1df96b15270081b3d8b10912
sayed <moka@facebook.com>
2023-06-21 14:22:47 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	_putchar('0');
+	_putchar('1');
+	_putchar('2');
+	_putchar('3');
+	_putchar('4');
+	_putchar('5');
+	_putchar('6');
+	_putchar('7');
+	_putchar('8');
+	_putchar('\n');
+	return 0;
+}
