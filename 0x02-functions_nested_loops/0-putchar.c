f1200bdba8ed5ec00692f8c62c098d1d04cbef50
sayed <moka@facebook.com>
2023-06-21 14:58:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void){
-	_putchar('0');
-	_putchar('1');
-	_putchar('2');
-	_putchar('3');
-	_putchar('4');
-	_putchar('5');
-	_putchar('6');
-	_putchar('7');
-	_putchar('8');
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return 0;
+
+	return (0);
 }
