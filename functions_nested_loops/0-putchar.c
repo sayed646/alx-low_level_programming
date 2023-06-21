d7105bc1dd6e5416e473e771ef271f322e5f9cfb
sayed <moka@facebook.com>
2023-06-21 14:10:46 -0700

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,20 @@
 #include <main.h>
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void){
-	putchat("_putchar\n");
-	return 0;
-	{
+	_putchar("0");
+		_putchar("1");
+		_putchar("2");
+		_putchar("3");
+		_putchar("4");
+		_putchar("5");
+		_putchar("6");
+		_putchar("7");
+		_putchar("/n");
+		retutn 0;
+}
+		
 
