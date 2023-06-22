878e9ad996abb04861dfe4bb98b63e4d8235863f
sayed <moka@facebook.com>
2023-06-22 00:13:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char m;
+    for(char m = 'a'; char m <= 'z'; m++);
+   _putchar('m');
+   _putchar("\n");
+	    
+    return (0);
+}
