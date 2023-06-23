56ed14176b467a8a6d9a78a233c5a07196a793b0
sayed <moka@facebook.com>
2023-06-23 16:19:21 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - uppercase letters
@@ -10,17 +9,9 @@
 int _isupper(int c)
 
 {
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 
-	char c;
-
-
-	c = 'A';
-
-	printf("%c: %d\n", c, _isupper(c));
-
-	c = 'a';
-
-	printf("%c: %d\n", c, _isupper(c));
-
-	return (0);
 }
