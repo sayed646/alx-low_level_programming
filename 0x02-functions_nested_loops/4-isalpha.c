8f5bbdcde23c4df71f2e39410f6e01e96b4d12b7
sayed <moka@facebook.com>
2023-06-23 00:39:11 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main - Write a function that checks for alphabetic character.
- *
+ * _isalpha - check the code for Holberton School students.
+ * @c: lower or uppercase alphabet
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
 	if(c <= 'a' && c >= 'z');
-	return(1);
+	return (1);
 	else if(c >= 'A' && c <= 'Z');
-	return(1);
+	return (1);
 	else
-		return(0);
+		return (0);
     return (0);
 }
