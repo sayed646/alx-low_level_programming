2e531529da10f422c47f4daf78d846cdf96ddfa7
sayed <moka@facebook.com>
2023-06-23 01:24:45 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @num: Integer input
+ * Return: Always 0.
+ */
+int print_last_digit(int num)
+{
+		num = num % 10;
+	if (num < 0)
+		num = num * -1;
+	_putchar(num + '0');
+	return (num);
+}
