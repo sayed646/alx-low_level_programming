fbe2c272a8ca49f8193f1222906f178114e38165
sayed <moka@facebook.com>
2023-06-23 01:53:28 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h
+
+/**
+ * print_to_98 - check the code for Holberton School students.
+ * @n: Integer to be inputted
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			else
+				printf("\n");
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			else
+				printf("\n");
+		}
+	}
+}
