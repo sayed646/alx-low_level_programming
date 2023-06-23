7782f3f1c7410396f6b937cd2f866774a9bc7a5c
sayed <moka@facebook.com>
2023-06-22 18:40:21 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@
 void print_remaining_days(int month, int day, int year)
 {
 
-	if ((year % 100 == 0 || year % 400 == 0) || !(year % 4 == 0))
+	if ((year % 100 == 0 || year % 400 == 0) || year % 4 == 0)
 
 	{
 
