5e6c809e614ae5703ad342abbe7dd6155964c896
sayed <moka@facebook.com>
2023-06-22 23:24:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+ int a = 0;
+    char letter = 'a';
+    while(a < 10)
+    while(letter <= 'z')
+    {
+    putchar(letter);
+    letter++;
+    {
+        putchar('\n');
+        a++;
+    }
+    }
+}
+    return (0);
+}
