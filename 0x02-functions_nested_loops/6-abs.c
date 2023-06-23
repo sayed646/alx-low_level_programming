91f7b0e89a0cbbad76e1617f9580685e1f55658f
sayed <moka@facebook.com>
2023-06-23 01:15:21 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code for Holberton School students.
+ * @num: Holds an integer
+ * Return: Always 0.
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		return (num * -1);
+	else
+		return (num);
+}
