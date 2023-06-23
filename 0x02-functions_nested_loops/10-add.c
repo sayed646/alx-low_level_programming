6441fcbde54c2811b37b933d3f2fd138a1863f2a
sayed <moka@facebook.com>
2023-06-23 01:51:52 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code for Holberton School students.
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
